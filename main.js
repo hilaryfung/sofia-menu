11c692276140633155c05da86d49a56bd1a430a2
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2025-02-25 17:19:59 -0800

    add discount functionality

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const lunchtime = document.querySelector("#lunchtime")
-const pricecheck = document.querySelector("#lunchtime")
+const pricecheck = document.querySelector("#pricecheck")
 const disclaimer = document.querySelector("#disclaimer")
 
 const lunchtimenote = function(){
     disclaimer.innerHTML="Croissants usually sell out at lunch so please check with your server!"
     
 }
-const discount1 = document.querySelector("#discount1")
-const discount2 = document.querySelector("#discount2")
-const discount3 = document.querySelector("#discount3")
-const onClickd = function(){
-    discount1.innerHTML = "30% off!"
-    discount2.innerHTML = "10% off!"
-    discount3.innerHTML = "50% off!"
+
+const discountedElements = document.querySelectorAll(".discount")
+const discounts = ["30% off!", "10% off!", "50% off!"]
+const onClickd = function() {
+
+    discountedElements.forEach((element, i) => {
+        element.innerHTML = discounts[i]
+    })
 }
 
 lunchtime.addEventListener("click", lunchtimenote)
